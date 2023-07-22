73b0a91029b1b43ed399e8673776b55483783ad8
Oom Lout <aaron@oomlout.com>
2023-07-23 00:03:43 +0100

    added another project

--- working.py
+++ working.py
@@ -2,6 +2,8 @@ import oomp_documentation_bot as oomp_bot
 
 
 def main():
+    ps = []
+
     project_deets = {}
     project_deets['github'] = 'https://github.com/electrolama/minik'
     project_deets['eagle_board_file_src'] = {}
@@ -10,7 +12,29 @@ def main():
     project_deets['eagle_schematic_file_src']['current'] = 'module/Revision A2/minik-RevA2.sch'
     #set the kicad src directory
     #project_deets['kicad_src_directory'] = 'module/Revision A2'
-    oomp_bot.document_project(**project_deets)
+    ps.append(project_deets)
+
+
+
+
+    project_deets = {}
+    project_deets['github'] = 'https://github.com/omerk/zig-a-zig-ah'
+    project_deets['eagle_board_file_src'] = {}
+    project_deets['eagle_board_file_src']['current'] = 'zzh/Revision A/zzh.brd'
+    project_deets['eagle_schematic_file_src'] = {}
+    project_deets['eagle_schematic_file_src']['current'] = 'zzh/Revision A/zzh.sch'
+    #set the kicad src directory
+    #project_deets['kicad_src_directory'] = {}
+    #project_deets['kicad_src_directory']["current"] = "oomp/current/working"
+    
+    'module/Revision A2'
+    ps.append(project_deets)
+
+
+
+
+    for project_deets in ps:
+        oomp_bot.document_project(**project_deets)
 
 
 
